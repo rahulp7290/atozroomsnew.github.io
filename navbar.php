9db582c1ae2a856753fc7774e8bd6af26502cafa
rahulp7290 <118588944+rahulp7290@users.noreply.github.com>
2024-05-17 13:03:16 +0530

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,19 @@
+! <nav class="navbar navbar-inverse"> 
+  <div class="container-fluid">
+  <div class="navbar-header">
+
+
+    <h1><a class="navbar-brand" href="index.php">Welcome to A-Z Accomdation </a></h1>
+</div>
+    <ul class="nav navbar-nav">
+       
+    <form class="d-flex" >
+        <a class="btn btn-outline-primary mx-2"  type="sumbit"  href="index.html"  > Home</a>
+        <a class="btn btn-outline-success mx-2" type="submit" href="signup.php" >Signup</a>
+        <a class="btn btn-outline-primary mx-2" type="submit" href="login.php">Login</a>
+        <a class="btn btn-outline-danger mx-2" type="submit" href="logout.php">Logout</a>
+    </form>
+    
+
+  </div>
+</nav>
